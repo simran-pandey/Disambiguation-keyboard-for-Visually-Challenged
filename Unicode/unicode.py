4cd0c4cbfbc00ced37856e543acce261a3b1c226
Simran Pandey <pandeysimran97@gmail.com>
2018-02-28 18:58:52 +0530

    Delete unicode.py

--- Unicode/unicode.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-# encoding: utf-8
-f1 = open('C:\\Users\\simran pandey\\Desktop\\IDC\\unicode\\dev.txt','r')# File containing the unicode representation of words in above file
-f2 = open('C:\\Users\\simran pandey\\Desktop\\IDC\\unicode\\uni.txt','w')
-for line in f1:
-	line1=list(line)
-	print line1
-	f2.write("%s" % line1)
